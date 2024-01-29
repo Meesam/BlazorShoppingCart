5bf08c2ea2c9cb4523b2289c898b498e05adde03
Meesam <meesam.engineer@gmail.com>
2024-01-29 09:47:19 +0530

    login and Register form working on backend part

---
+++ BlazorShoppingCart.Models/ViewModel/LoginVM.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorShoppingCart.Models.ViewModel
+{
+    public class LoginVM
+    {
+        [Required(ErrorMessage = "Username is required.")]
+        [DataType(DataType.EmailAddress)]
+        public string? Username { get; set; }
+
+        [Required(ErrorMessage = "Password is required.")]
+        [DataType(DataType.Password)]
+        public string? Password { get; set; }
+
+        public bool RememberMe { get; set; }
+
+    }
+}
---
+++ BlazorShoppingCart.Models/ViewModel/RegisterVM.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorShoppingCart.Models.ViewModel
+{
+    public class RegisterVM
+    {
+        [Required(ErrorMessage = "Name is required")]
+        public string? Name { get; set; }
+
+        [Required(ErrorMessage = "Email is required")]
+        [DataType(DataType.EmailAddress)]
+        public string? Email { get; set; }
+
+        [Required(ErrorMessage = "Dob is required")]
+        [DisplayName("Dob")]
+        public DateTime DateOfBirth { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        [DataType(DataType.Password)]
+        public string? Password { get; set; }
+
+        [Compare("Password", ErrorMessage = "Password do not match")]
+        [DisplayName("Confirm password")]
+        public string? ConfirmPassword { get; set; }
+    }
+}
